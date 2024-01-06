373febe22da69eb3953bb504ed69de1ac08ee0bb
Marothi Mahlake <code_artist@outlook.com>
2024-01-06 21:21:42 +0200

    Algoritim: Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using SequentialAccessCollections;
+
+namespace BubbleSort;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Collection numbers = new();
+        Random random = new(100);
+
+        for (int i = 0; i <= 10; i++)
+            numbers.Add(random.Next() * 10);
+
+        foreach (object number in numbers)
+            Console.WriteLine(number);
+
+        numbers.BubbleSort(); // Bubble sort is the slowest sorting algorithm.
+        Console.WriteLine("____________________________________________");
+
+        foreach (object number in numbers)
+            Console.WriteLine(number);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
--- SequentialAccessCollections/Collection.cs
+++ SequentialAccessCollections/Collection.cs
@@ -23,5 +23,24 @@ namespace SequentialAccessCollections
         {
             return InnerList.Count;
         }
+
+        public void BubbleSort()
+        {
+            int upper = Count() - 1;
+            object temp;
+
+            for(int outer = upper; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer -1; inner++)
+                {
+                    if ((int)InnerList[inner] > (int)InnerList[inner + 1])
+                    {
+                        temp = InnerList[inner];
+                        InnerList[inner] = InnerList[inner + 1];
+                        InnerList[inner + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
