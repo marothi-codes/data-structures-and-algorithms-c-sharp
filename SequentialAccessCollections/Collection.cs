59a1b014cf44191b8435b99cfc851b935bc4f39d
Marothi Mahlake <code_artist@outlook.com>
2024-01-06 16:12:52 +0200

    Sequential Access Collections

---
+++ SequentialAccessCollections/Collection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace SequentialAccessCollections
+{
+	public class Collection : CollectionBase
+	{
+
+		public void Add(Object item)
+		{
+			InnerList.Add(item);
+		}
+
+		public void Remove(Object item)
+		{
+			InnerList.Remove(item);
+		}
+
+		public new void Clear()
+		{
+			InnerList.Clear();
+		}
+
+		public new int Count()
+		{
+			return InnerList.Count;
+		}
+
+	}
+}
---
+++ SequentialAccessCollections/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace SequentialAccessCollections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Declare and initialise a collection
+        Collection courses = new()
+        {
+            "JavaScript",
+            "TypeScript",
+            ".Net",
+            "Angular",
+            "Azure DevOps",
+            "PHP",
+            "Python",
+            "Rust"
+        };
+
+        // Print each collecton item in the console
+        foreach (var course in courses)
+        {
+            Console.WriteLine(course);
+        }
+
+        // Print the number of items in the collection
+        Console.WriteLine($"Number of courses: {courses.Count()}");
+
+        // Remove 1 item from the collection
+        courses.Remove("PHP");
+
+        // Print the number of items in the collection
+        Console.WriteLine($"Number of courses remaining: {courses.Count()}");
+
+        // Empty the collection
+        courses.Clear();
+
+        // Print the number of items in the collection
+        Console.WriteLine($"Number of courses remaining: {courses.Count()}");
+
+        // Prompt the user to press any key to exit and await input
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+    }
+}
