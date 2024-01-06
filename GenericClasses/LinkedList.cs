14de2c710e9fa03b691b5450301643feccd047ee
Marothi Mahlake <code_artist@outlook.com>
2024-01-06 20:32:23 +0200

    Generics

---
+++ GenericClasses/LinkedList.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericClasses
+{
+	public class LinkedList<T>
+	{
+		public T Data { get; set; }
+		public LinkedList<T>? List { get; set; }
+
+		public LinkedList(T data, LinkedList<T>? linkedList)
+		{
+			Data = data;
+			List = linkedList;
+		}
+
+	}
+}
---
+++ GenericClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericClasses;
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<string> node1 = new("John", null);
+        LinkedList<string> node2 = new("Matt", node1);
+        LinkedList<string>? obj = node2.List;
+
+        Console.WriteLine(node1.Data);
+        Console.WriteLine(node2.Data);
+        Console.WriteLine(obj?.Data);
+
+        Console.ReadKey();
+    }
+
+    static void Swap<T>(ref T val1, ref T val2)
+    {
+        T temp;
+        temp = val1;
+        val1 = val2;
+        val2 = temp;
+    }
+}
