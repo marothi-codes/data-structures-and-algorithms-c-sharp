a4f04580a6598ecfcce9f7309dffde483afbec48
Marothi Mahlake <code_artist@outlook.com>
2024-01-07 09:45:56 +0200

    FIX: Namespace change

--- RandomNumberGenerator/Random.cs
+++ RandomNumberGenerator/Random.cs
@@ -1,18 +1,20 @@
-﻿namespace RandomNumberGenerator;
+﻿using System.Security.Cryptography;
+
+namespace CustomRandomNumberGenerator;
 
 public class Random
 {
     public static int NextInt32()
     {
         var byteArray = new byte[4];
-        System.Security.Cryptography.RandomNumberGenerator.Fill(byteArray);
+        RandomNumberGenerator.Fill(byteArray);
         return BitConverter.ToInt32(byteArray, 0);
     }
 
     public static double NextDouble()
     {
         var byteArray = new byte[8];
-        System.Security.Cryptography.RandomNumberGenerator.Fill(byteArray);
+        RandomNumberGenerator.Fill(byteArray);
         ulong randomValue = BitConverter.ToUInt64(byteArray, 0);
         return (double)randomValue / ulong.MaxValue; // Normalize to [0, 1]
     }
@@ -20,7 +22,7 @@ public class Random
     public static float NextSingle()
     {
         var byteArray = new byte[4];
-        System.Security.Cryptography.RandomNumberGenerator.Fill(byteArray);
+        RandomNumberGenerator.Fill(byteArray);
         uint randomValue = BitConverter.ToUInt32(byteArray, 0);
         return (float)randomValue / uint.MaxValue; // Normalize to [0, 1]
     }
