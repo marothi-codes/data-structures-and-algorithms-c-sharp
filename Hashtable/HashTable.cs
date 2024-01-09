cf4eee33dacdd84557abc172d717d9fecc89bfc2
Marothi Mahlake <code_artist@outlook.com>
2024-01-09 11:46:47 +0200

    HashTable Implementation

---
+++ Hashtable/HashTable.cs
@@ -0,0 +1,75 @@
+﻿#nullable disable
+
+namespace HashTable;
+
+public class HashTable<TKey, TValue>
+{
+    private LinkedList<KeyValuePair<TKey, TValue>>[] buckets;
+
+    public HashTable(int size)
+    {
+        buckets = new LinkedList<KeyValuePair<TKey, TValue>>[size];
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        int bucket = GetBucket(key);
+        if (buckets[bucket] == null)
+            buckets[bucket] = new LinkedList<KeyValuePair<TKey, TValue>>();
+
+        buckets[bucket].AddLast(new KeyValuePair<TKey, TValue>(key, value));
+    }
+
+    public bool Remove(TKey key)
+    {
+        int bucket = GetBucket(key);
+        var node = Find(key);
+        if (node != null)
+        {
+            buckets[bucket].Remove(node);
+            return true;
+        }
+        return false;
+    }
+
+    public TValue Get(TKey key)
+    {
+        var node = Find(key);
+        if (node != null)
+            return node.Value.Value;
+        return default;
+    }
+
+    public void PrintHashTable()
+    {
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            if (buckets[i] != null)
+            {
+                foreach (var keyValuePair in buckets[i])
+                {
+                    Console.WriteLine($"Key: {keyValuePair.Key}, Value: {keyValuePair.Value}");
+                }
+            }
+        }
+    }
+
+    private LinkedListNode<KeyValuePair<TKey, TValue>> Find(TKey key)
+    {
+        int bucket = GetBucket(key);
+        if (buckets[bucket] != null)
+        {
+            for (var node = buckets[bucket].First; node != null; node = node.Next)
+            {
+                if (node.Value.Key.Equals(key))
+                    return node;
+            }
+        }
+        return null;
+    }
+
+    private int GetBucket(TKey key)
+    {
+        return Math.Abs(key.GetHashCode()) % buckets.Length;
+    }
+}
---
+++ Hashtable/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HashTable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashTable<string, string> hashTable = new(10);
+
+        // Adding key-value pairs to the hash table
+        hashTable.Add("Marothi", "Software Engineer");
+        hashTable.Add("Bob", "Data Scientist");
+        hashTable.Add("Charlie", "Product Manager");
+
+        // Getting values from the hash table
+        Console.WriteLine(hashTable.Get("Marothi")); // Output: Software Engineer
+        Console.WriteLine(hashTable.Get("Bob")); // Output: Data Scientist
+        Console.WriteLine(hashTable.Get("Charlie")); // Output: Product Manager
+
+        Console.WriteLine("Printing the hash table...");
+
+        hashTable.PrintHashTable();
+
+        // Removing a key-value pair from the hash table
+        Console.WriteLine("Removing 'Marothi' from the hash table");
+        hashTable.Remove("Marothi");
+
+        Console.WriteLine("Printing the hash table...");
+
+        hashTable.PrintHashTable();
+
+        // Trying to get a value for a key that has been removed
+        Console.WriteLine(hashTable.Get("Marothi")); // Output: null
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
