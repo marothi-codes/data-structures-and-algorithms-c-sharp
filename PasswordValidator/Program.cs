a7ee1480bcadc80518fb2c591ebfb2464e46cda8
Marothi Mahlake <code_artist@outlook.com>
2024-01-17 09:14:54 +0200

    Password Validator implementation

---
+++ PasswordValidator/Program.cs
@@ -0,0 +1,44 @@
+﻿#nullable disable
+
+namespace PasswordValidator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a complex password");
+        string password = Console.ReadLine();
+
+        if (IsPasswordNullOrEmpty(password))
+            Console.WriteLine("The password is required");
+        else if (password.Length < 8)
+        {
+            Console.WriteLine("Your password needs to be 8 characters or longer.");
+        }
+        else if (IsPasswordValid(password))
+        {
+            Console.WriteLine("The password is valid as contains letters, numbers and special characters. Well Done!");
+        }
+        else
+        {
+            Console.WriteLine("Your password is not complex enough...");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static bool IsPasswordValid(string password)
+    {
+        return password.Any(char.IsUpper) &&
+            password.Any(char.IsLower) &&
+            password.Any(char.IsLetterOrDigit) &&
+            password.Any(char.IsSymbol);
+    }
+
+    static bool IsPasswordNullOrEmpty(string password)
+    {
+
+        return string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(password);
+    }
+}
